9c49ba9e8cccfdb94cb678487b6093caf591eee4
Artem721 <71816794+Artem721@users.noreply.github.com>
2020-10-22 13:01:11 +0300

    Add files via upload

--- ft_find _char.py
+++ ft_find _char.py
@@ -1,27 +1,27 @@
-def ft_len(sstr):
-    l = 0
-    for i in sstr:
-        l += 1
-    return l
+def ft_len(a):
+    r = 0
+    for i in a:
+        r += 1
+    return r
 
 
-def ft_find_char(char, sstr):
+def ft_find_char(char, a):
     count = 0
     x = 0
     r = 0
-    if char in sstr:
-        for i in range(ft_len(sstr)):
-            if sstr[i] == char:
+    if char in a:
+        for i in range(ft_len(a)):
+            if a[i] == char:
                 x = i
                 break
-        for i in sstr:
+        for i in a:
             if i == char:
                 count += 1
         if count == 1:
             return x
         else:
-            for i in range(ft_len(sstr)):
-                if sstr[i] == char:
+            for i in range(ft_len(a)):
+                if a[i] == char:
                     r = i
             return x, r
     else:
--- ft_reverse_between_char.py
+++ ft_reverse_between_char.py
@@ -1,48 +1,48 @@
-def ft_len(stsr):
-    q = 0
-    for i in stsr:
-        q += 1
-    return q
+def ft_len(s):
+    r = 0
+    for i in s:
+        r += 1
+    return r
 
 
-def ft_find_char(char, str):
+def ft_find_char(char, t):
     count = 0
     x = 0
     r = 0
-    if char in str:
-        for i in range(ft_len(str)):
-            if str[i] == char:
+    if char in t:
+        for i in range(ft_len(t)):
+            if t[i] == char:
                 x = i
                 break
-        for i in str:
+        for i in t:
             if i == char:
                 count += 1
         if count == 1:
             return x
         else:
-            for i in range(ft_len(str)):
-                if str[i] == char:
+            for i in range(ft_len(t)):
+                if t[i] == char:
                     r = i
             return x, r
     else:
         return False
 
 
-def ft_reverse_str(str):
+def ft_reverse_str(t):
     a = ''
-    for i in range(-1, -ft_len(str) - 1, -1):
-        a += str[i]
+    for i in range(-1, -ft_len(t) - 1, -1):
+        a += t[i]
     return a
 
 
-def ft_reverse_between_char(char, stsr):
+def ft_reverse_between_char(char, r):
     b = 0
-    for i in stsr:
+    for i in r:
         if i == char:
             b += 1
     if b == 0:
         return -2
     elif b == 1:
         return -1
-    a, c = ft_find_char(char, stsr)
-    return ft_reverse_str(stsr[a + 1:c])
+    a, c = ft_find_char(char, r)
+    return ft_reverse_str(r[a + 1:c])
