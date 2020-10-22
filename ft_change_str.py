d5ea118d516512765bd94d4d9fcc254ac0094c3e
Artem721 <71816794+Artem721@users.noreply.github.com>
2020-10-22 13:37:56 +0300

    Add files via upload

--- ft_change_str.py
+++ ft_change_str.py
@@ -1,18 +1,18 @@
-def ft_len(a):
-    b = 0
-    for i in a:
-        b += 1
-    return b
+def ft_len(stsr):
+    l = 0
+    for i in stsr:
+        l += 1
+    return l
 
 
 def ft_change_str(str1, str2, str3):
     a = ''
-    n = 0
+    ind = 0
     if str3.find(str1) == -1:
         return False
     else:
         while str3.find(str1) != -1:
-            n = str3.find(str1)
-            a = a + str3[:n] + str2
-            str3 = str3[n + ft_len(str1):]
+            ind = str3.find(str1)
+            a = a + str3[:ind] + str2
+            str3 = str3[ind + ft_len(str1):]
     return a + str3
